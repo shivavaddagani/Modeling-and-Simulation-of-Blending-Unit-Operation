0b68e4f05ed5bab2a271cf4d5f0cf18d6e1b7db9
Shiva Vaddagani <shivagoud.v@gmail.com>
2021-04-29 01:06:59 +0530

    Add files via upload

---
+++ Matlab_files/xzy_revised.m
@@ -0,0 +1,62 @@
+%% Old method
+% for ii1=1:Nx
+%     for Kk1=1:Nz 
+%         for jj1=1:Ny  
+%             if(numb(ii1,jj1,Kk1)>0)
+%                 numb1=numb1+1;
+%                 numb_new(numb1)=numb(ii1,jj1,Kk1);
+%                 count_a_new(numb1)=count_a(ii1,jj1,Kk1);
+%                 mass_a(numb1)=count_a(ii1,jj1,Kk1)*M1;% mass of particle type A
+%                 count_b_new(numb1)=count_b(ii1,jj1,Kk1);
+%                 mass_b(numb1)=count_b(ii1,jj1,Kk1)*M2;% mass of particle type B
+%                 tot_mass(numb1)=mass_a(numb1)+mass_b(numb1);% TOTAL MASS
+%                 inew(numb1)=ii1;
+%                 jnew(numb1)=jj1;
+%                 knew(numb1)=Kk1;
+%             end
+%         end
+%     end
+% end
+%%
+
+%% New Method
+
+Nx=60; Ny=60; Nz=30;
+k2=0;j2=0;i2=0;
+  
+   for ii1=1:Nx
+      
+      i2 = ii1;
+      if(ii1>1)
+          k2 = k2 + ((-1)^(ii1));
+      end
+      
+      for Kk1=1:Nz
+          k2 = k2 + ((-1)^(ii1+1));
+          if(ii1>1)||(Kk1>1)
+              %j2 = j2 + ((-1)^(ii1+1))*((-1)^(Kk1));
+                j2 = j2 + ((-1)^(Kk1));
+          end
+          
+          for jj1=1:Ny
+            %  j2 = j2 + ((-1)^(ii1+1))*((-1)^(Kk1+1));
+              j2 = j2 + ((-1)^(Kk1+1));
+              
+              if(numb(i2,j2,k2)>0)
+                  numb1=numb1+1;
+                  numb_new(numb1)=numb(i2,j2,k2);
+                  count_a_new(numb1)=count_a(i2,j2,k2);
+                  mass_a(numb1)=count_a(i2,j2,k2)*M1;% mass of particle type A
+                  count_b_new(numb1)=count_b(i2,j2,k2);
+                  mass_b(numb1)=count_b(i2,j2,k2)*M2;% mass of particle type B
+                  tot_mass(numb1)=mass_a(numb1)+mass_b(numb1);% TOTAL MASS
+                  inew(numb1)=i2;
+                  jnew(numb1)=j2;
+                  knew(numb1)=k2;
+              end
+                %            [i2 j2 k2]
+          end
+                %     pause
+      end
+   end
+ 
